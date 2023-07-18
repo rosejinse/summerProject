0b45ce2ffafe1b61ad56fc8ccb179a9c42aa7f73
Bang <owen1743@naver.com>
2023-07-18 22:31:41 +0900

    Swagger 설정

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Tag(name = "Hello", description = "테스트용 API")
+@RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
+    @Operation(summary = "say HELLO!", description = "HELLO! 출력")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "SUCCESS"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+
+    })
+    @GetMapping("/api/helloo")
     @ResponseBody
     public String sayHello(){
         return "HELLO!";
---
+++ demo/src/main/java/com/example/demo/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("hello")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SWE_Project")
+                        .description("학과 서비스 프로젝트 API"));
+    }
+}
